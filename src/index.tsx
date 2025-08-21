496ff03b30e74655d8bf831ff9219afbd50f9282
Sanooj Es <sanoojes6371@gmail.com>
2025-08-21 19:53:34 +0530

    lint: src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,17 @@
-import addPageData from "@app/features/addPageData.ts";
-import addPlayerData from "@app/features/addPlayerData.ts";
-import addUMV from "@app/features/addUMV.tsx";
-import setBackground from "@app/features/setBackground.tsx";
-import setControls from "@app/features/setControls.ts";
-import setPlayer from "@app/features/setPlaybar.tsx";
-import waitForGlobal from "@app/utils/dom/waitForGlobal.ts";
-import initNotificationSystem from "@app/utils/initNotificationSystem.tsx";
-import patchIcons from "@app/utils/patchIcons.ts";
-import { isWindows } from "@app/utils/platform.ts";
-import addSettings from "@app/utils/settings/addSettings.tsx";
+import addPageData from '@app/features/addPageData.ts';
+import addPlayerData from '@app/features/addPlayerData.ts';
+import addUMV from '@app/features/addUMV.tsx';
+import setBackground from '@app/features/setBackground.tsx';
+import setControls from '@app/features/setControls.ts';
+import setPlayer from '@app/features/setPlaybar.tsx';
+import waitForGlobal from '@app/utils/dom/waitForGlobal.ts';
+import initNotificationSystem from '@app/utils/initNotificationSystem.tsx';
+import patchIcons from '@app/utils/patchIcons.ts';
+import { isWindows } from '@app/utils/platform.ts';
+import addSettings from '@app/utils/settings/addSettings.tsx';
 
 const main = async () => {
-  await waitForGlobal(
-    () => Spicetify?.Platform && Spicetify?.React && Spicetify?.ReactDOM
-  );
+  await waitForGlobal(() => Spicetify?.Platform && Spicetify?.React && Spicetify?.ReactDOM);
   await new Promise((res) => Spicetify?.Events?.webpackLoaded?.on(res));
 
   initNotificationSystem();
