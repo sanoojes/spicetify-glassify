a979a41180826440ae9889f4757156571e4ef78e
Sanooj Es <sanoojes6371@gmail.com>
2025-09-19 16:05:20 +0530

    feat: add user id with analytics

--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,9 @@
-export const createLogger = (prefix: string = "") => ({
-  info: (...args: any[]) => console.log("[Glassify]", prefix, ...args),
-  debug: (...args: any[]) => console.debug("[Glassify]", prefix, ...args),
-  warn: (...args: any[]) => console.trace("[Glassify]", prefix, ...args),
-  trace: (...args: any[]) => console.trace("[Glassify]", prefix, ...args),
-  error: (...args: any[]) => console.error("[Glassify]", prefix, ...args),
+export const createLogger = (prefix: string = '') => ({
+  info: (...args: any[]) => console.log('[Glassify]', prefix, ...args),
+  debug: (...args: any[]) => console.debug('[Glassify]', prefix, ...args),
+  warn: (...args: any[]) => console.trace('[Glassify]', prefix, ...args),
+  trace: (...args: any[]) => console.trace('[Glassify]', prefix, ...args),
+  error: (...args: any[]) => console.error('[Glassify]', prefix, ...args),
 });
 
 export const logger = createLogger();
